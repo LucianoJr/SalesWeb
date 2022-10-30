0f7c02d294d29c1e1de5d55f15a507885a187cac
Luciano Jr <luciano.juniorgiovani@gmail.com>
2022-10-30 13:01:31 -0300

    Tratando chave estrangeira e adicionando select de departamento

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -4,21 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWeb.Services;
+using SalesWeb.Models;
 
 namespace SalesWeb.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerViewModel { Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
